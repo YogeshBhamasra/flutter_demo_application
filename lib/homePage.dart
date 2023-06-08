955fe038adfa25fccdd92e548f9d5516b92d2bbb
Yogesh <yogesh.rao@bigohtech.com>
2023-06-08 11:26:33 +0530

    Day 2 completed

---
+++ lib/homePage.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const String name = "Demo App";
+    BorderSide borderSide = const BorderSide(
+      color: Colors.black,
+      width: 1.5,
+      style: BorderStyle.solid,
+    );
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(name),
+      ),
+      drawer: Drawer(),
+      body: SafeArea(
+        child: Container(
+          padding: const EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            color: Colors.amber,
+            border: Border(
+              top: borderSide,
+              bottom: borderSide,
+              left: borderSide,
+              right: borderSide,
+            ),
+            borderRadius: const BorderRadius.all(
+              Radius.circular(8),
+            ),
+          ),
+          child: const Text("New"),
+        ),
+      ),
+    );
+  }
+}
